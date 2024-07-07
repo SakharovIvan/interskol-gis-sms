15ec813657be54b3c5431a52b4cf01adec3a81e1
i.sakharov <i731901@gmail.com>
2024-07-07 22:48:19 +0300

    12345

--- bin/sntSMS.js
+++ bin/sntSMS.js
@@ -1,6 +1,6 @@
-import sentmail from "../email/sentfile.js";
+
 import { emailConfig } from "../config.js";
-import { createMasSMS, normalizeTlf } from "../src/sentSMS.js";
+
 
 const mcday = 86400000;
 const mchour = 3600000;
@@ -13,58 +13,3 @@ const logger = log.createSimpleLogger({
 });
 logger.setLevel(emailConfig.logs.level || "debug");
 
-const sentsmsmas = async (massms) => {
-  try {
-    if ((await massms[0].length) > 0) {
-      logger.info(normalizeTlf(massms[0]));
-      const massms0 = normalizeTlf(massms[0]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        `${massms0}`,
-        emailConfig.SMTPSentcliSMS.textprin
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayPrinMail ", err);
-  }
-
-  try {
-    if ((await massms[1].length) > 0) {
-      logger.info(normalizeTlf(massms[1]));
-      const massms1 = normalizeTlf(massms[1]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        `${massms1}`,
-        emailConfig.SMTPSentcliSMS.textvipoln
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayVipolnMail ", err);
-  }
-
-  try {
-    if ((await massms[2].length) > 0) {
-      logger.info(normalizeTlf(massms[2]));
-      const massms2 = normalizeTlf(massms[2]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        `${massms2}`,
-        emailConfig.SMTPSentcliSMS.textopros
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayVipolnMail ", err);
-  }
-};
-
-export default () => {
-  setInterval(async () => {
-    try {
-      const massms = await createMasSMS();
-      await sentsmsmas(massms);
-    } catch (err) {
-      console.log(err);
-      logger.info(err);
-    }
-  }, mchour + mchour / 60);
-};
--- bin/startfile.js
+++ bin/startfile.js
@@ -1,6 +1,6 @@
 import sentmail from "../email/sentfile.js";
 import { emailConfig } from "../config.js";
-import sentSMS from "./sntSMS.js";
+import { createMasSMS, normalizeTlf } from "../src/sentSMS.js";
 import updateGISbd from "../src/JSONtoSQLbd.js";
 import createJSONfromXLSX from "../src/emailDataToJSON.js";
 import createGISreport from "../src/GISbdtoXLSX.js";
@@ -31,6 +31,7 @@ setInterval(async () => {
   try {
     const json = createJSONfromXLSX("i.sakharov_LLWarranty17062024");
     await updateGISbd(json);
+    await createGISreport();
   } catch (err) {
     console.log(err);
     logger.info(err);
@@ -39,7 +40,6 @@ setInterval(async () => {
 
 setInterval(async () => {
   try {
-    await createGISreport();
     await sentmail(
       emailConfig.SMTPSentreport.emailto,
       emailConfig.SMTPSentreport.subject,
@@ -58,9 +58,56 @@ setInterval(async () => {
   }
 }, mchour + mchour / 60);
 
-try {
-  sentSMS();
-} catch (err) {
-  console.log(err);
-  logger.info(err);
-}
+const sentsmsmas = async (massms) => {
+  try {
+    if ((await massms[0].length) > 0) {
+      logger.info(normalizeTlf(massms[0]));
+      const massms0 = normalizeTlf(massms[0]);
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        `${massms0}`,
+        emailConfig.SMTPSentcliSMS.textprin
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayPrinMail ", err);
+  }
+
+  try {
+    if ((await massms[1].length) > 0) {
+      logger.info(normalizeTlf(massms[1]));
+      const massms1 = normalizeTlf(massms[1]);
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        `${massms1}`,
+        emailConfig.SMTPSentcliSMS.textvipoln
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayVipolnMail ", err);
+  }
+
+  try {
+    if ((await massms[2].length) > 0) {
+      logger.info(normalizeTlf(massms[2]));
+      const massms2 = normalizeTlf(massms[2]);
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        `${massms2}`,
+        emailConfig.SMTPSentcliSMS.textopros
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayVipolnMail ", err);
+  }
+};
+
+setInterval(async () => {
+  try {
+    const massms = await createMasSMS();
+    await sentsmsmas(massms);
+  } catch (err) {
+    console.log(err);
+    logger.info(err);
+  }
+}, mchour/20);
