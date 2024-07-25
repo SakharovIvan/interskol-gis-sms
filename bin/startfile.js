dbb28d5d5002c74b5789770ca4b515af0743cb48
i.sakharov <i731901@gmail.com>
2024-07-25 21:06:44 +0300

    schedule added

--- bin/startfile.js
+++ bin/startfile.js
@@ -5,6 +5,7 @@ import updateGISbd from "../src/JSONtoSQLbd.js";
 import createJSONfromXLSX from "../src/emailDataToJSON.js";
 import createGISreport from "../src/GISbdtoXLSX.js";
 import getPost from "../email/imap_readfile.js";
+import { RecurrenceRule, scheduleJob } from "node-schedule";
 
 const mcday = 86400000;
 const mchour = 3600000;
@@ -12,18 +13,84 @@ const week = mcday * 7;
 const emailReport = "a.rogov@kls-gr.ru; i.sakharov@kls-gr.ru";
 import log from "simple-node-logger";
 
-//const normalizeTlf = (tlf) => {
-//  return `${tlf}`.replace(/\)/g, "")
-//  .replace(/\(/g, "")
-//  .replace(/ /g, "")
-//  .replace(/\+/g, "")
-//};
-
 const logger = log.createSimpleLogger({
   logFilePath: "logger.log",
   timestampFormat: "YYYY-MM-DD HH:mm:ss.SSS",
 });
 logger.setLevel(emailConfig.logs.level || "debug");
+
+const smsrule = new RecurrenceRule();
+smsrule.dayOfWeek = [1, 5];
+smsrule.hour = 18;
+smsrule.minute = 0;
+
+const reportrule = new RecurrenceRule();
+smsrule.dayOfWeek = [1, 5];
+smsrule.hour = 18;
+smsrule.minute = 0;
+
+const sentsmsmas = async (massms) => {
+  try {
+    if ((await massms[0].length) > 0) {
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        normalizeTlf(massms[0]),
+        emailConfig.SMTPSentcliSMS.textprin
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayPrinMail ", err);
+  }
+
+  try {
+    if ((await massms[1].length) > 0) {
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        normalizeTlf(massms[1]),
+        emailConfig.SMTPSentcliSMS.textvipoln
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayVipolnMail ", err);
+  }
+
+  try {
+    if ((await massms[2].length) > 0) {
+      await sentmail(
+        emailConfig.SMTPSentcliSMS.emailto,
+        normalizeTlf(massms[2]),
+        emailConfig.SMTPSentcliSMS.textopros
+      );
+    }
+  } catch (err) {
+    logger.info("tlfArrayOprosMail ", err);
+  }
+};
+
+scheduleJob(smsrule, function () {
+  sentsmsmas();
+});
+
+scheduleJob(reportrule, function () {
+  try {
+    sentmail(
+      emailConfig.SMTPSentreport.emailto,
+      emailConfig.SMTPSentreport.subject,
+      "SomeText",
+      "GISdata.xlsx"
+    ).then(() => {
+      logger.info(
+        `GIS report sent ${
+          (emailConfig.SMTPSentreport.emailto,
+          emailConfig.SMTPSentreport.subject)
+        }`
+      );
+    });
+  } catch (err) {
+    console.log(err);
+    logger.info(err);
+  }
+});
 
 setInterval(async () => {
   try {
@@ -45,76 +112,32 @@ setInterval(async () => {
   }
 }, mchour / 6);
 
-setInterval(async () => {
-  try {
-    await sentmail(
-      emailConfig.SMTPSentreport.emailto,
-      emailConfig.SMTPSentreport.subject,
-      "SomeText",
-      "GISdata.xlsx"
-    );
+//setInterval(async () => {
+//  try {
+//    await sentmail(
+//      emailConfig.SMTPSentreport.emailto,
+//      emailConfig.SMTPSentreport.subject,
+//      "SomeText",
+//      "GISdata.xlsx"
+//    );
+//
+//    logger.info(
+//      `GIS report sent ${
+//        (emailConfig.SMTPSentreport.emailto, emailConfig.SMTPSentreport.subject)
+//      }`
+//    );
+//  } catch (err) {
+//    console.log(err);
+//    logger.info(err);
+//  }
+//}, mchour * 6);
 
-    logger.info(
-      `GIS report sent ${
-        (emailConfig.SMTPSentreport.emailto, emailConfig.SMTPSentreport.subject)
-      }`
-    );
-  } catch (err) {
-    console.log(err);
-    logger.info(err);
-  }
-}, mchour * 6);
-
-const sentsmsmas = async (massms) => {
-  try {
-    if ((await massms[0].length) > 0) {
-      // logger.info(normalizeTlf(massms[0]));
-      // const massms0 = normalizeTlf(massms[0]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        normalizeTlf(massms[0]),
-        emailConfig.SMTPSentcliSMS.textprin
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayPrinMail ", err);
-  }
-
-  try {
-    if ((await massms[1].length) > 0) {
-      // logger.info(normalizeTlf(massms[1]));
-      // const massms1 = normalizeTlf(massms[1]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        normalizeTlf(massms[1]),
-        emailConfig.SMTPSentcliSMS.textvipoln
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayVipolnMail ", err);
-  }
-
-  try {
-    if ((await massms[2].length) > 0) {
-      //logger.info(normalizeTlf(massms[2]));
-      //const massms2 = normalizeTlf(massms[2]);
-      await sentmail(
-        emailConfig.SMTPSentcliSMS.emailto,
-        normalizeTlf(massms[2]),
-        emailConfig.SMTPSentcliSMS.textopros
-      );
-    }
-  } catch (err) {
-    logger.info("tlfArrayOprosMail ", err);
-  }
-};
-
-setInterval(async () => {
-  try {
-    const massms = await createMasSMS();
-    await sentsmsmas(massms);
-  } catch (err) {
-    console.log(err);
-    logger.info(err);
-  }
-}, mchour / 60);
+//setInterval(async () => {
+//  try {
+//    const massms = await createMasSMS();
+//    await sentsmsmas(massms);
+//  } catch (err) {
+//    console.log(err);
+//    logger.info(err);
+//  }
+//}, mchour / 60);
