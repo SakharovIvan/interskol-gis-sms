6ce3f288a5cbe73756aaba5be200a2ef843d738c
i.sakharov <i731901@gmail.com>
2024-08-01 13:42:34 +0300

    1

--- bin/startfile.js
+++ bin/startfile.js
@@ -11,13 +11,14 @@ const mcday = 86400000;
 const mchour = 3600000;
 const week = mcday * 7;
 const emailReport = "a.rogov@kls-gr.ru; i.sakharov@kls-gr.ru";
-//import log from "simple-node-logger";
-//
-//const logger = log.createSimpleLogger({
-//  logFilePath: "logger.log",
-//  timestampFormat: "YYYY-MM-DD HH:mm:ss.SSS",
-//});
-//logger.setLevel(emailConfig.logs.level || "debug");
+import log from "simple-node-logger";
+
+const logger = log.createSimpleLogger({
+  logFilePath: "logger.log",
+  timestampFormat: "YYYY-MM-DD HH:mm:ss.SSS",
+});
+logger.setLevel(emailConfig.logs.level || "debug");
+logger.info("Process", err);
 
 const smsrule = new RecurrenceRule();
 smsrule.dayOfWeek = [new Range(1,5)];
