e0382fe2183f0aed0e1ef7bc0a3c670669eae55c
SakharovIvan <i731901@gmail.com>
2025-10-30 16:10:41 +0300

    work if no work

--- src/models/repair.dto.js
+++ src/models/repair.dto.js
@@ -85,13 +85,14 @@ export class Repair {
     try {
       const cur_work = this.SP_list.filter((el) => el.work);
       if (cur_work[0].spmatNo) {
+        console.log(cur_work[0].spmatNo)
         this.work = (
           await Work_DB.findOne({ where: { gis_code: Number(cur_work[0].spmatNo) } })
         ).dataValues;
       }
-      this.work = null;
+      this.work = {gis_code:101};
     } catch (error) {
-      this.work = null;
+      this.work = {gis_code:101};
     }
   }
 
